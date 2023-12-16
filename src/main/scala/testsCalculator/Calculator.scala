4675efdf8ad9f13f7c4e2dd7fc673b92edcffdaa
erminvermin <erminvermin@outlook.com>
2023-12-16 16:12:40 +0300

    My scala project

---
+++ src/main/scala/testsCalculator/Calculator.scala
@@ -0,0 +1,14 @@
+package testsCalculator
+
+class Calculator {
+
+  def add(a: Int, b: Int): Int = a + b
+
+  def subtract(a: Int, b: Int): Int = a - b
+
+  def multiply(a: Int, b: Int): Int = a * b
+
+  def divide(a: Int, b: Int): Int = a / b
+}
+
+object Calculator
